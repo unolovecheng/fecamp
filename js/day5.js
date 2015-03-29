c9095489e8fa7c11467364d77acbe1c0244eee7f
unolovecheng <unolovecheng@gmail.com>
2015-03-29 16:16:22 +0900

    comiit 6

--- js/day5.js
+++ js/day5.js
@@ -6,12 +6,12 @@ function age(y, m, d){
     var 생일 = new Date(y, m-1, d);
     var 오늘 = new Date();
     var 만나이 = 오늘.getFullYear() - y - 1;
-    if(오늘.getMonth() >= m && 오늘.getDate() >= d){
+    if(오늘.getMonth()+1 >= m && 오늘.getDate() >= d){
         만나이 += 1;
     }
     return 만나이;
 }
-console.log(age(1986, 11, 26));
+console.log(age(2014, 3, 28));
 
 function 연령대(y, m, d){
     var 만나이 = age(y,m,d);
